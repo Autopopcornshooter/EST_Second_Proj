822d63e09833cd997603adf91c3ffb241d5f8457
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-09-23 14:15:54 +0900

    Add: 프로젝트 초기 세팅 - build.gradle 의존성 추가 - application 환경변수 추가   - DB는 postgreSQL로 통일
    TODO: .env 환경변수 개인DM으로 공유

---
+++ Baemin-Manager/src/test/java/EST/Baemin/Manager/BaeminManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package EST.Baemin.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
