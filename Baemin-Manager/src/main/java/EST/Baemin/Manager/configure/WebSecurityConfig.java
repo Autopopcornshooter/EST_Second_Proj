2c5bbaf18373d9cb4f57591e903a920c02d928c2
Autopopcornshooter <70250405+Autopopcornshooter@users.noreply.github.com>
2025-09-29 10:20:47 +0900

    feat: 구글 로그인 연동 구현완료 및 페이지 버튼 생성

--- Baemin-Manager/src/main/java/EST/Baemin/Manager/configure/WebSecurityConfig.java
+++ Baemin-Manager/src/main/java/EST/Baemin/Manager/configure/WebSecurityConfig.java
@@ -37,16 +37,24 @@ private final CustomOAuth2UserService customOAuth2UserService;
     //배포용 보안 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeHttpRequests(auth->
+        httpSecurity
+                .authorizeHttpRequests(auth->
                 auth.requestMatchers("/home","/login","/signup").permitAll()// 로그인, 회원가입은 인증 없이 접근 가능
                         .requestMatchers(toH2Console()).permitAll()
                         .requestMatchers("/.well-known/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(auth-> auth.loginPage("/login")
                         .defaultSuccessUrl("/api/regions",true))
-                .logout(auth->auth.logoutSuccessUrl("/login")
+                .oauth2Login(oauth2->oauth2
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/api/regions",true)
+                        .userInfoEndpoint(userInfo->userInfo
+                                .userService(customOAuth2UserService)))// 구글 로그인 DB 연동 처리
+                .logout(auth->auth
+                        .logoutSuccessUrl("/login")
                         .invalidateHttpSession(true)
                         .clearAuthentication(true));
+
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console())); // ✅ CSRF 무시
         httpSecurity.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())); // ✅ frame 허용
         return httpSecurity.build();
