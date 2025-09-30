65a6b102f241a578bffcd63a2d3dd29976d12196
Tayo Bus <wnsqja6694@gmail.com>
2025-09-30 17:56:07 +0900

    Fix: "엔티티 수정에 따른 DTO 수정"

--- Baemin-Manager/src/main/java/EST/Baemin/Manager/dto/ChatRoomResponse.java
+++ Baemin-Manager/src/main/java/EST/Baemin/Manager/dto/ChatRoomResponse.java
@@ -2,8 +2,10 @@ package EST.Baemin.Manager.dto;
 
 import EST.Baemin.Manager.domain.Chat;
 import EST.Baemin.Manager.domain.ChatRoom;
+import EST.Baemin.Manager.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -12,19 +14,24 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChatRoomResponse {
   private long id;
-  private long user1Id;
-  private long user2Id;
-  private LocalDateTime updatedAt;
+  private User user1;
+  private User user2;
   private List<Chat> chats;
+  private String lastChat;
 
-  public ChatRoomResponse(ChatRoom chatRoom) {
-    this.id = chatRoom.getId();
-    this.user1Id = chatRoom.getUser1Id();
-    this.user2Id = chatRoom.getUser2Id();
-    this.updatedAt = chatRoom.getUpdatedAt();
-    this.chats = chatRoom.getChats();
+  public static ChatRoomResponse from(ChatRoom chatRoom) {
+    ChatRoomResponse response = new ChatRoomResponse();
+    response.setId(chatRoom.getId());
+    response.setUser1(chatRoom.getUser1());
+    response.setUser2(chatRoom.getUser2());
+    response.setChats(chatRoom.getChats());
+    if (!chatRoom.getChats().isEmpty()) {
+      response.setLastChat(chatRoom.getChats().get(chatRoom.getChats().size() - 1).getMessage());
+    }
+    return response;
   }
 }
