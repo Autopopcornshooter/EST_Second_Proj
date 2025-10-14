f5b17f6c81333f85497dff10bebda202b8a76989
Tayo Bus <wnsqja6694@gmail.com>
2025-10-14 09:17:58 +0900

    Feat: "관리자 페이지 유저부분 API 추가 "

--- Baemin-Manager/src/main/java/EST/Baemin/Manager/service/UserService.java
+++ Baemin-Manager/src/main/java/EST/Baemin/Manager/service/UserService.java
@@ -2,10 +2,16 @@ package EST.Baemin.Manager.service;
 
 import EST.Baemin.Manager.dto.AddUserRequest;
 import EST.Baemin.Manager.domain.User;
+import EST.Baemin.Manager.dto.UserResponse;
 import EST.Baemin.Manager.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -24,4 +30,13 @@ public class UserService {
         );
     }
 
+    public Page<UserResponse> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable)
+                       .map(UserResponse::from);
+    }
+
+    public Page<UserResponse> searchUsersByName(String keyword, Pageable pageable) {
+        return userRepository.findByNicknameContaining(keyword, pageable)
+                       .map(UserResponse::from);
+    }
 }
